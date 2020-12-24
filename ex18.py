3dd1344e39af3f0846ab6fd355781be6f1a13f4b
Shefali Vishwakarma <62350473+shefcode@users.noreply.github.com>
2020-12-24 11:13:01 -0800

    Create ex18.py

---
+++ ex18.py
@@ -0,0 +1,18 @@
+# Exercise 18 - Names, Variables, Code Functions
+def print_two(*args):
+	arg1, arg2 = args
+	print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_two_again(arg1,arg2):
+	print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_one(arg1):
+	print(f"arg1: {arg1}")
+
+def print_none():
+	print("I got nothin'.")
+
+print_two("Shefali", "Vishwakarma")
+print_two_again("Shefali", "Vishwakarma")
+print_one("First!")
+print_none()
